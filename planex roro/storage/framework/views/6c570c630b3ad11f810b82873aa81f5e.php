83eb8caa064629c31951f4bf38218a10f8b8e8ec
Mael Aumont <maelroinac8@gmail.com>
2026-06-01 13:53:06 +0200

    Remove 'planex roro' directory

--- planex roro/storage/framework/views/6c570c630b3ad11f810b82873aa81f5e.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-    <p>© <?php echo e(date('Y')); ?> PlanEx - Tous droits réservés</p>
-</footer><?php /**PATH /var/www/planex/resources/views/partials/footer.blade.php ENDPATH**/ ?>
